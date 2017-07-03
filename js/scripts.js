b510322623e85af4faf93a1125d04a68a2b4bc3e
Bart Webber and Paul Guevarra <bart@>
2017-07-03 14:45:26 -0700

    add boostrap.css, add clickable JQuery functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+JQuery("h1").click(function() {
+  alert("this is a header.");
+});
+JQuery("p").click(function() {
+  alert("this is a paragraph.");
+});
+JQuery("img").click(function(){
+  alert("This is an image")
+});
